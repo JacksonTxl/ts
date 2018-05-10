c1a8e9dbe2c25d936057280fdd372d647c818717
JacksonTxl <172004133@qq.com>
2018-05-10 08:53:59 +0800

    add lib component

---
+++ projects/base/src/lib/base.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BaseComponent } from './base.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [BaseComponent],
+  exports: [BaseComponent]
+})
+export class BaseModule { }
