7063c6b55490fd6e3c0de34f9f4245bb6ebdf2da
Hiker <172004133@qq.com>
2018-05-11 09:00:28 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {CustomerModuleModule} from './customer-module/customer-module.module';
+import { CustomerModuleModule } from './customer-module/customer-module.module';
 
 @NgModule({
   declarations: [
