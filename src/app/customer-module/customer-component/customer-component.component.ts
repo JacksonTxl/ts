9370bc9a850e872a76334867a1a35fe771e5c19e
Hiker <172004133@qq.com>
2018-05-11 14:16:46 +0800

    add log service

--- src/app/customer-module/customer-component/customer-component.component.ts
+++ src/app/customer-module/customer-component/customer-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogService } from '../../services/log.service';
 
 @Component({
   selector: 'app-customer-component',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
+    this.logService.log('test logService');
   }
 
 }
